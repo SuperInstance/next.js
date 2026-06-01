e9ff11d1b4f6e16b51991b650156840c27763740
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-06-01 10:44:16 +0200

    DEBUG enabled by default

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -2062,6 +2062,7 @@ export const defaultConfig = Object.freeze({
     turbopackFileSystemCacheForBuild: false,
     turbopackInferModuleSideEffects: true,
     turbopackPluginRuntimeStrategy: 'childProcesses',
+    durableUseCacheEntries: true,
   },
   htmlLimitedBots: undefined,
   bundlePagesRouterDependencies: false,
